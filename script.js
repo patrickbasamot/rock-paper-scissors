9bf6ecc35c569f3933ab6ca23cb2822d3aeb67f9
Patrick Basamot <patrickbasamot@gmail.com>
2022-03-06 14:12:47 +0800

    Added function playgame

--- script.js
+++ script.js
@@ -11,6 +11,26 @@ computerPlay(choice);
 
 let playerSelection = prompt('What is your Weapon \'Rock\',\'Paper\',\'Scissors\'?');
 let computerSelection = computerPlay(choice);
+function playRound (playerSelection,computerSelection)  {
+    playerSelection === computerSelection ?  console.log('Draw'):
+    playerSelection === 'Scissors' && computerSelection[choice] === 'Paper'? console.log('You win'):
+    playerSelection === 'Paper' && computerSelection[choice] === 'Rock'? console.log('You win'):
+    playerSelection === 'Rock' && computerSelection[choice] === 'Scissors'? console.log('You win'):
 
-console.log(computerSelection,playerSelection);
+    playerSelection === 'Paper' && computerSelection[choice] === 'Scissor'? console.log('The Computer Won'):
+    playerSelection === 'Rock' && computerSelection[choice] === 'Paper'? console.log('The Computer Won'):
+    playerSelection === 'Scissors' && computerSelection[choice] === 'Rock'? console.log('The Computer Won'):
+    
+
+    console.log('Failed');
+
+
+}
+
+
+
+console.log(`The player chose ${playerSelection}`);
+console.log(`The computer chose ${computerPlay(choice)}`);
+
+console.log(playRound(playerSelection, computerSelection));
 
