d20533d54d1d5bd27011ed242fbb37be79bb34da
Patrick Basamot <patrickbasamot@gmail.com>
2022-03-06 18:15:41 +0800

    Fix the bug

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 
-const choice = ['Rock','Paper','Scissors'];
-let computerPlay =(choice) => { 
-    
+function game(){
+    for(let i = 0; i < 5; i++){
+   const choice = ['Rock','Paper','Scissors'];
+   
+  
+   let computerPlay =(choice) => { 
     const randomChoice = Math.floor(Math.random() * choice.length);
     return choice[randomChoice];
 
 } 
 
 computerPlay(choice);
-
 let playerSelection = prompt('What is your Weapon \'Rock\',\'Paper\',\'Scissors\'?');
 let computerSelection = computerPlay(choice);
-function playRound (playerSelection,computerSelection)  {
-    playerSelection === computerSelection ?  console.log('Draw'):
-    playerSelection === 'Scissors' && computerSelection === 'Paper'? console.log('You win, The Scissor beats Paper'):
-    playerSelection === 'Paper' && computerSelection === 'Rock'? console.log('You win, The Paper beats Rock'):
-    playerSelection === 'Rock' && computerSelection === 'Scissors'? console.log('You win, The Rock beats Paper'):
 
+let playerScore =0;
+let computerScore =0;
+
+function playRound (playerSelection,computerSelection,playerScore,computerScore)  {
+
+    
+    playerSelection === computerSelection ?  console.log('Draw'):
+   
+    playerSelection === 'Scissors' && computerSelection === 'Paper'? console.log('You win, The Scissor beats Paper'):
+    playerSelection === 'Paper' && computerSelection === 'Rock'? console.log('You win, The Paper beats Rock') :
+    playerSelection === 'Rock' && computerSelection === 'Scissors'? console.log('You win, The Rock beats Scissors'):
 
     playerSelection === 'Paper' && computerSelection === 'Scissors'? console.log('The Computer Win, The scissor beats Paper'):
-    playerSelection === 'Rock' && computerSelection === 'Paper'? console.log('The Computer Win, The Paper beats Rock'):
-    playerSelection === 'Scissors' && computerSelection === 'Rock'? console.log('The Computer Won, The Rock beats Paper'):
-    
+    playerSelection === 'Rock' && computerSelection === 'Paper'? console.log('The Computer Win, The Paper beats Rock') :
 
+    playerSelection === 'Scissors' && computerSelection === 'Rock'? console.log('The Computer Won, The Rock beats Paper'):
     console.log('Failed');
 
 
@@ -30,8 +37,16 @@ function playRound (playerSelection,computerSelection)  {
 
 
 
-console.log(`You chose ${playerSelection}`);
-console.log(`The computer chose ${computerSelection}`);
+console.log(`You chose ${playerSelection},The computer chose ${computerSelection}`);
 
-console.log(playRound(playerSelection, computerSelection));
 
+
+playRound(playerSelection, computerSelection);
+
+
+
+    
+    }
+}
+
+game();
