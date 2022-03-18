0f6e7d7b8826ac5b13d893c7f96fcb78bd1a957d
Patrick Basamot <patrickbasamot@gmail.com>
2022-03-18 22:08:06 +0800

    Added More Functions for the game

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function game(){
 } 
 
 computerPlay(choice);
-//let playerSelection = prompt('What is your Weapon \'Rock\',\'Paper\',\'Scissors\'?');
+let playerSelection = prompt('What is your Weapon \'Rock\',\'Paper\',\'Scissors\'?');
 let computerSelection = computerPlay(choice);
 
 let playerScore =0;
@@ -25,6 +25,7 @@ function playRound (playerSelection,computerSelection)  {
     playerSelection === 'Scissors' && computerSelection === 'Paper'? console.log('You win, The Scissor beats Paper'):
     playerSelection === 'Paper' && computerSelection === 'Rock'? console.log('You win, The Paper beats Rock') :
     playerSelection === 'Rock' && computerSelection === 'Scissors'? console.log('You win, The Rock beats Scissors'):
+
 
     playerSelection === 'Paper' && computerSelection === 'Scissors'? console.log('The Computer Win, The scissor beats Paper'):
     playerSelection === 'Rock' && computerSelection === 'Paper'? console.log('The Computer Win, The Paper beats Rock') :
@@ -49,4 +50,77 @@ playRound(playerSelection, computerSelection);
     
 }
 
-game();
+//game();
+
+
+const  click = () => {
+    //Buttons
+    const rockBtn = document.querySelector('.rock');
+    const  paperBtn = document.querySelector('.paper');
+    const  scissorBtn = document.querySelector('.scissor');
+
+  //Score boards
+    let playerScoreBoard = document.querySelector('.p-count');
+    let playerScore = 0;
+    let computerScoreBoard = document.querySelector('.c-count');
+    let computerScore = 0;
+
+    const playerChoice = [rockBtn,paperBtn,scissorBtn];
+    const computerChoice = ['rock','paper','scissor'];
+    const result = document.querySelector('.result');
+    playerChoice.forEach(option => {
+        option.addEventListener('click',function(){
+
+
+            const choiceNumber = Math.floor(Math.random()* computerChoice.length);
+            const computerOption = computerChoice[choiceNumber];
+
+                if( option === computerChoice){
+                    result.textContent = 'Draw';
+                } 
+                else if (option == scissorBtn &&  computerOption === 'paper'  ){
+                    result.textContent = " You win";
+                    playerScore++;
+                    playerScoreBoard.textContent = playerScore;
+                 }
+                else if(option === paperBtn && computerOption ==='rock'){
+                    result.textContent = " You win";
+                    playerScore++;
+                    playerScoreBoard.textContent = playerScore;
+                }
+                else if(option === rockBtn && computerOption === 'rock'){
+                    result.textContent = " You win";
+                    playerScore++;
+                    playerScoreBoard.textContent = playerScore;
+                }
+                else if( option === paperBtn && computerOption === 'scissor'){
+                        result.textContent = "Computer Win";
+                        computerScore++;
+                        computerScoreBoard.textContent = computerScore;
+                }
+                else if( option === rockBtn && computerOption === 'paper'){
+                    result.textContent = "Computer Win";
+                    computerScore++;
+                    computerScoreBoard.textContent = computerScore;
+                }
+                else if( option === scissorBtn && computerOption === 'rock'){
+                    result.textContent = "Computer Win";
+                    computerScore++;
+                    computerScoreBoard.textContent = computerScore;
+                }
+
+                if(playerScore > 3  ){
+                    alert('You win');
+                    window.location.reload();
+                }
+                else if(computerScore > 3 ){
+                    alert('Computer Win');
+                    window.location.reload();
+                }
+            })
+        })
+    }
+
+
+
+click();
